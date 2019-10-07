c52411beb028e25ab7e095b4709d53fc32a44845
KarDeMumman <sarah.walid.alsaadi@liu.se>
2019-10-07 11:20:21 +0200

    commented out everything

--- Knapsack/R/Knapsack.R
+++ Knapsack/R/Knapsack.R
@@ -1,8 +1,8 @@
 #Generate the data
 
-set.seed(42)
-n<-20 
-knapsack_object<-data.frame(w=sample(1:40,size = n,replace = TRUE), v=runif(n=n,0,10))
+#set.seed(42)
+#n<-20 
+#knapsack_object<-data.frame(w=sample(1:40,size = n,replace = TRUE), v=runif(n=n,0,10))
 
 #knapsack_brute_force<-function(x,W)
 #{
