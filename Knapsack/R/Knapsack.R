5c824a79588f9561cc4aaeb5d1a9632f9f79c3b7
KarDeMumman <sarah.walid.alsaadi@liu.se>
2019-10-13 23:47:30 +0200

    Add the greedy solution.

--- Knapsack/R/Knapsack.R
+++ Knapsack/R/Knapsack.R
@@ -42,7 +42,7 @@ brute_force_knapsack<-function(x,W)
 }
 
 
-#' Solve the 0-1 knapsack problem (dynamic progeamming approach)
+#' Solve the 0-1 knapsack problem (dynamic programming approach)
 #' @param x A dataframe with weights (w) and values (v).
 #' @param W the knapsack size.
 #' @return a list with the maximum value and the indexes of the elements that compose this optimal solution 
@@ -111,3 +111,46 @@ dynamic_knapsack<-function(x,W)
   
   return(sol)
 }
+
+
+#' Solve the 0-1 knapsack problem (greedy approach)
+#' @param x A dataframe with weights (w) and values (v).
+#' @param W the knapsack size.
+#' @return a list with the maximum value and the indexes of the elements that compose this optimal solution 
+
+greedy_knapsack<-function(x,W)
+{
+  stopifnot(is.data.frame(x))
+  stopifnot(length(x) == 2)
+  stopifnot(names(x) == c("w", "v"))
+  stopifnot(W>=0)
+  
+  w<-x[,1]
+  v<-x[,2]
+  
+  stopifnot(all(w > 0))
+  stopifnot(all(v > 0))
+  
+  best_elements<-c()
+  
+  rate <- v/w
+  
+  ranked_rate <- order(rate,decreasing = TRUE)
+  
+  best_weight <- 0
+  best_value <- 0
+  
+  for (i in ranked_rate){
+    if(w[i] + best_weight <= W){
+      best_weight <- best_weight + w[i]
+      best_value <- best_value + v[i]
+      best_elements <- c(best_elements, i)
+    }
+    else if(w[i] + best_weight > W){
+      break
+    }
+    
+  }
+  return(list(value=best_value, elements=best_elements))
+}
+
